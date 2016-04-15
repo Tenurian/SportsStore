960a1f2a2a75fb090ff54c6be6f014e81dfc73d3
Thomas Feuerborn <Thomas Feuerborn>
2016-04-15 17:13:32 -0600

    Added ImagePath to database, Product.cs, and updated the view to show the image
    Signed-off-by: Thomas Feuerborn <Thomas Feuerborn>

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -13,5 +13,6 @@ namespace SportsStore.Domain.Entities
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public string ImagePath { get; set; }
     }
 }
