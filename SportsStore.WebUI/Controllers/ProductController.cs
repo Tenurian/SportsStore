0659719f228c271fca66e4f22fcef2c420ae1b85
Thomas Feuerborn <Thomas Feuerborn>
2016-04-18 16:19:56 -0600

    added pagination, adding more items to database to allow the filter to display it's working properly
    Signed-off-by: Thomas Feuerborn <Thomas Feuerborn>

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,14 +11,21 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
         // GET: Product
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ActionResult List()
+
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize));
         }
+
+        //public ActionResult List()
+        //{
+        //    return View(repository.Products);
+        //}
     }
 }
