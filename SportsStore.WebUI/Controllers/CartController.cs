11a301095a665b1dbdd9021ede6c8b9ab0354135
Thomas Feuerborn <Thomas Feuerborn>
2016-04-19 03:10:35 -0600

    Finished Chapter 8
    Signed-off-by: Thomas Feuerborn <Thomas Feuerborn>

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportsStore.WebUI.Models;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 
@@ -15,6 +16,14 @@ namespace SportsStore.WebUI.Controllers
         public CartController(IProductRepository repo)
         {
             repository = repo;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
         }
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
@@ -44,10 +53,6 @@ namespace SportsStore.WebUI.Controllers
                 Session["Cart"] = cart;
             }
             return cart;
-        }
-        public ActionResult Index()
-        {
-            return View();
         }
     }
 }
